5a2f6de8428f5740fdd4552abb592679b8131f6a
Hasini Sankalpana <156077759+Hasini-Sankalpana@users.noreply.github.com>
2024-12-23 14:18:21 +0530

    fixed error

--- src/lib/mongodb/mongoose.js
+++ src/lib/mongodb/mongoose.js
@@ -11,7 +11,7 @@ export const connect = async () => {
 
     try {
         await mongoose.connect(process.env.MONGODB_URI, {
-            dbName:'Blogify',
+            dbName:'Blogify App',
             useUNewUrlParser: true,
             useUnifiedTopology: true,
         });
