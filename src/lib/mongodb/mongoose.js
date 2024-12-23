acb457eddf21a73c8afa31d5ef78ef9b33956d79
Hasini Sankalpana <156077759+Hasini-Sankalpana@users.noreply.github.com>
2024-12-23 14:49:40 +0530

    fixed the mongodb error

--- src/lib/mongodb/mongoose.js
+++ src/lib/mongodb/mongoose.js
@@ -1,22 +1,22 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 let initialized = false;
 
 export const connect = async () => {
-    mongoose.set('strictQuery',true);
-    if (initialized) {
-        console.log("Already connected to MongoDB");
-        return;
-    }
-
-    try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-            dbName:'Blogify App',
-            useUNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log("Connected to MongoDB");
-    } catch (error) {
-        console.error("Error connecting to MongoDB", error);
-    }
-}
+  mongoose.set('strictQuery', true);
+  if (initialized) {
+    console.log('Already connected to MongoDB');
+    return;
+  }
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: 'Blogify',
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to MongoDB');
+    initialized = true;
+  } catch (error) {
+    console.log('Error connecting to MongoDB:', error);
+  }
+};
