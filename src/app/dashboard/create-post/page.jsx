521d17483413ab81cd54d62d92e2e06afd971021
Hasini Sankalpana <156077759+Hasini-Sankalpana@users.noreply.github.com>
2024-12-24 11:16:03 +0530

    complete upload-post functionality

--- src/app/dashboard/create-post/page.jsx
+++ src/app/dashboard/create-post/page.jsx
@@ -8,12 +8,17 @@ import ProgressBar from "@ramonak/react-progress-bar"; // Import progress bar
 
 const ReactQuill = dynamic(() => import('react-quill-new'), { ssr: false });
 import 'react-quill-new/dist/quill.snow.css';
+import { useRouter } from 'next/navigation';
 
 export default function CreatePostPage() {
   const { isSignedIn, user, isLoaded } = useUser();
   const [imageFile, setImageFile] = useState(null);
   const [imageURL, setImageURL] = useState('');
   const [uploadProgress, setUploadProgress] = useState(0);
+  const [formData, setFormData] = useState({});
+  const [publishError, setPublishError] = useState(null);
+  const router = useRouter();
+  console.log(formData);
 
   if (!isLoaded) {
     return null;
@@ -31,6 +36,10 @@ export default function CreatePostPage() {
 
       try {
         const uploadedImage = await uploadImageToCloudinary(imageFile, setUploadProgress);
+        setFormData((prevFormData) => ({
+          ...prevFormData,
+          image: uploadedImage.secure_url, // Add the image URL to formData
+        }));
         setImageURL(uploadedImage.secure_url);
        // alert('Image uploaded successfully!');
         setTimeout(() => {
@@ -42,12 +51,40 @@ export default function CreatePostPage() {
       }
     };
 
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      try {
+        const res = await fetch('/api/post/create', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            ...formData,
+            userMongoId: user.publicMetadata.userMongoId,
+          }),
+        });
+        const data = await res.json();
+        if (!res.ok) {
+          setPublishError(data.message);
+          return;
+        }
+        if (res.ok) {
+          setPublishError(null);
+          router.push(`/post/${data.slug}`);
+        }
+      } catch (error) {
+        setPublishError('Something went wrong');
+      }
+    };
+  
+
     return (
       <div className='p-3 max-w-3xl mx-auto min-h-screen'>
         <h1 className='text-center text-3xl my-7 font-semibold'>
           Create a post
         </h1>
-        <form className='flex flex-col gap-4'>
+        <form className='flex flex-col gap-4'  onSubmit={handleSubmit}>
           <div className='flex flex-col gap-4 sm:flex-row justify-between'>
             <TextInput
               type='text'
@@ -55,8 +92,15 @@ export default function CreatePostPage() {
               required
               id='title'
               className='flex-1'
+              onChange={(e) =>
+                setFormData({ ...formData, title: e.target.value })
+              }
             />
-            <Select>
+            <Select
+             onChange={(e) =>
+              setFormData({ ...formData, category: e.target.value })
+            }
+            >
               <option value='uncategorized'>Select a category</option>
               <option value='javascript'>JavaScript</option>
               <option value='reactjs'>React.js</option>
@@ -92,6 +136,9 @@ export default function CreatePostPage() {
             placeholder='Write something...'
             className='h-72 mb-12'
             required
+            onChange={(value) => {
+              setFormData({ ...formData, content: value });
+            }}
           />
           <Button type='submit' gradientDuoTone='purpleToPink'>
             Publish
